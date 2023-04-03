ca2de712318ffed7f1231396dfde061fd740855b
Teerachai Laothong <tlaothong@gmail.com>
2023-04-03 07:55:22 +0700

    Added FooterBand

--- src/components/layouts/default/index.ts
+++ src/components/layouts/default/index.ts
@@ -1,3 +1,5 @@
 import HeaderBandVue from "./HeaderBand.vue";
+import FooterBandVue from "./FooterBand.vue";
 
 export const HeaderBand = HeaderBandVue;
+export const FooterBand = FooterBandVue;
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,11 +6,12 @@
       <slot></slot>
     </main>
 
+    <FooterBand></FooterBand>
   </div>
 </template>
 
 <script setup lang="ts">
-import { HeaderBand } from '~/components/layouts/default';
+import { HeaderBand, FooterBand } from '~/components/layouts/default';
 import { initFlowbite } from 'flowbite/lib/esm/components';
 
 onMounted(() => { initFlowbite() })
