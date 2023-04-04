9af5560170d8565939a35d7f21ef3a50d3c5c748
Teerachai Laothong <tlaothong@gmail.com>
2023-04-04 23:21:53 +0700

    Set min h in list EngSingle

--- src/pages/EntSingle/list.vue
+++ src/pages/EntSingle/list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-x-auto">
+  <div class="overflow-x-auto min-h-sm">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
