f82dd515469dc413fbab1f3b2ac0bd177f70415e
Teerachai Laothong <tlaothong@gmail.com>
2023-04-04 22:34:38 +0700

    Use menu in index (eng single)

--- src/pages/EntSingle/index.vue
+++ src/pages/EntSingle/index.vue
@@ -1,6 +1,65 @@
 <template>
-  <button type="button"
-    @click="navigateTo('/EntSingle/list')"
-    class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    List</button>
+  <aside aria-label="Related articles"
+    class="py-8 lg:py-12 bg-white dark:bg-gray-900">
+    <div class="px-4 mx-auto max-w-screen-xl">
+      <h2 class="mb-8 text-2xl font-bold text-gray-900 dark:text-white">What can I do for you?</h2>
+      <article class="flex">
+        <NuxtLink href="#"
+          class="shrink-0">
+          <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png"
+            class="mr-5 w-32 h-32 max-w-full align-middle rounded-full"
+            alt="Image 3">
+        </NuxtLink>
+        <div class="flex flex-col justify-center">
+          <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+            <NuxtLink href="#">Search</NuxtLink>
+          </h2>
+          <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+            changes! After months of preparation.</p>
+          <NuxtLink href="#"
+            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+            I want this
+          </NuxtLink>
+        </div>
+      </article>
+      <article class="flex mt-8">
+        <NuxtLink to="EntSingle/list"
+          class="shrink-0">
+          <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png"
+            class="mr-5 w-32 h-32 max-w-full align-middle rounded-full"
+            alt="Image 2">
+        </NuxtLink>
+        <div class="flex flex-col justify-center">
+          <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+            <NuxtLink to="/EntSingle/list">List Data</NuxtLink>
+          </h2>
+          <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+            changes! After months of preparation.</p>
+          <NuxtLink to="EntSingle/list"
+            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+            I want this
+          </NuxtLink>
+        </div>
+      </article>
+      <article class="flex mt-8">
+        <a href="#"
+          class="shrink-0">
+          <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png"
+            class="mr-5 w-32 h-32 max-w-fullalign-middle rounded-full"
+            alt="Image 1">
+        </a>
+        <div class="flex flex-col justify-center">
+          <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+            <a href="#">Create a New One</a>
+          </h2>
+          <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+            changes! After months of preparation.</p>
+          <a href="#"
+            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+            I want this
+          </a>
+        </div>
+      </article>
+    </div>
+  </aside>
 </template>
