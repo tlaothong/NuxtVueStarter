cacbad2273a8958e32aaddcaeba2d86d10fb6727
Teerachai Laothong <tlaothong@gmail.com>
2023-04-04 07:50:31 +0700

    Basic content structure

--- src/pages/EntSingle/index.vue
+++ src/pages/EntSingle/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Index</h1>
+  <button type="button"
+    @click="navigateTo('/EntSingle/list')"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    List</button>
 </template>
