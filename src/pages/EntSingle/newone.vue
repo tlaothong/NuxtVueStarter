66f7e3951d1d973612080189257c10d08055ba23
Teerachai Laothong <tlaothong@gmail.com>
2023-04-05 23:46:41 +0700

    Use GrowTextarea in newone

--- src/pages/EntSingle/newone.vue
+++ src/pages/EntSingle/newone.vue
@@ -51,10 +51,10 @@
             <div class="sm:col-span-2">
               <label for="biography"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Biography</label>
-              <textarea id="biography"
-                rows="8"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Your description here"></textarea>
+              <GrowTextarea id="biography"
+                v-model="dummy"
+                rows="4"
+                placeholder="Your description here"></GrowTextarea>
             </div>
           </div>
           <button type="submit"
@@ -69,4 +69,5 @@
 
 <script setup lang="ts">
 useSearchBar({ hide: true })
+const dummy = ref('')
 </script>
