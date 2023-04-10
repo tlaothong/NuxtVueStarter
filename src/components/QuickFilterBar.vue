f176fe96b43da414f30095b582fcaa238345bf08
Teerachai Laothong <tlaothong@gmail.com>
2023-04-10 11:45:34 +0700

    Decompose to the components

---
+++ src/components/QuickFilterBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="mx-4 pb-3 flex flex-wrap">
+    <div class="hidden md:flex items-center text-sm font-medium text-gray-900 dark:text-white mr-4 mt-3">Show only:
+    </div>
+    <div class="flex flex-wrap">
+      <div class="flex items-center mt-3 mr-4">
+        <input id="all-tasks"
+          type="radio"
+          value=""
+          name="show-only"
+          class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="all-tasks"
+          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">All</label>
+      </div>
+      <div class="flex items-center mr-4 mt-3">
+        <input id="completed"
+          type="radio"
+          value=""
+          name="show-only"
+          class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="completed"
+          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Completed tasks</label>
+      </div>
+      <div class="flex items-center mr-4 mt-3">
+        <input id="in-progress"
+          type="radio"
+          value=""
+          name="show-only"
+          class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="in-progress"
+          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tasks in progress</label>
+      </div>
+      <div class="flex items-center mr-4 mt-3">
+        <input id="in-review"
+          type="radio"
+          value=""
+          name="show-only"
+          class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="in-review"
+          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tasks in review</label>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/EntSingle/list.vue
+++ src/pages/EntSingle/list.vue
@@ -3,126 +3,20 @@
 
     <ClientOnly>
       <Teleport to="#module-top-bar">
-
         <TopBar></TopBar>
-
       </Teleport>
     </ClientOnly>
 
-    <div class="mx-4 pb-3 flex flex-wrap">
-      <div class="hidden md:flex items-center text-sm font-medium text-gray-900 dark:text-white mr-4 mt-3">Show only:
-      </div>
-      <div class="flex flex-wrap">
-        <div class="flex items-center mt-3 mr-4">
-          <input id="all-tasks"
-            type="radio"
-            value=""
-            name="show-only"
-            class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-          <label for="all-tasks"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">All</label>
-        </div>
-        <div class="flex items-center mr-4 mt-3">
-          <input id="completed"
-            type="radio"
-            value=""
-            name="show-only"
-            class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-          <label for="completed"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Completed tasks</label>
-        </div>
-        <div class="flex items-center mr-4 mt-3">
-          <input id="in-progress"
-            type="radio"
-            value=""
-            name="show-only"
-            class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-          <label for="in-progress"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tasks in progress</label>
-        </div>
-        <div class="flex items-center mr-4 mt-3">
-          <input id="in-review"
-            type="radio"
-            value=""
-            name="show-only"
-            class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-          <label for="in-review"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tasks in review</label>
-        </div>
-      </div>
-    </div>
+    <QuickFilterBar></QuickFilterBar>
 
     <Suspense>
-
       <InfoList></InfoList>
-
       <template #fallback>
         Loading ...
       </template>
     </Suspense>
 
-    <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-      aria-label="Table navigation">
-      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
-        Showing
-        <span class="font-semibold text-gray-900 dark:text-white">1-10</span>
-        of
-        <span class="font-semibold text-gray-900 dark:text-white">1000</span>
-      </span>
-      <ul class="inline-flex items-stretch -space-x-px">
-        <li>
-          <a href="#"
-            class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            <span class="sr-only">Previous</span>
-            <svg class="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewbox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd" />
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-        </li>
-        <li>
-          <a href="#"
-            aria-current="page"
-            class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
-        </li>
-        <li>
-          <a href="#"
-            class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            <span class="sr-only">Next</span>
-            <svg class="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewbox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd" />
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <ListFooterPaging></ListFooterPaging>
 
     <NuxtPage></NuxtPage>
 
