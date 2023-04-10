fc91f77c4cdc8d543e3252d92280ecba1fbb0626
Teerachai Laothong <tlaothong@gmail.com>
2023-04-11 02:13:06 +0700

    Use schema in edit

--- src/pages/EntSingle/[id]/edit.vue
+++ src/pages/EntSingle/[id]/edit.vue
@@ -1,59 +1,12 @@
 <template>
   <section class="bg-white dark:bg-gray-900">
-    <div class="max-w-2xl pb-8 mx-auto lg:pb-12">
+    <div class="max-w-2xl pb-6 mx-auto lg:pb-8">
       <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Update user</h2>
-      <form @submit.prevent="$router.go(-1)">
-        <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-6">
-          <div>
-            <label for="first-name"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-            <input type="text"
-              name="first-name"
-              id="first-name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              value="Helene"
-              placeholder="Ex. John">
-          </div>
-          <div class="w-full">
-            <label for="last-name"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-            <input type="text"
-              name="last-name"
-              id="last-name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              value="Engels"
-              placeholder="Ex. Doe">
-          </div>
-          <div>
-            <label for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password"
-              name="password"
-              id="password"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              value=""
-              placeholder="•••••••••">
-          </div>
-          <div>
-            <label for="confirm-password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-            <input type="password"
-              name="confirm-password"
-              id="confirm-password"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              value=""
-              placeholder="•••••••••">
-          </div>
-          <div class="sm:col-span-2">
-            <label for="biography"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Biography</label>
-            <textarea id="biography"
-              rows="8"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Write biography here...">Hello, I'm Helene Engels, USA Designer, Creating things that stand out, Featured by Adobe, Figma, Webflow and others, Daily design tips & resources, Exploring Web3.</textarea>
-          </div>
-        </div>
-        <div class="flex items-center space-x-4">
+      <FormRoot @submit="$router.go(-1)">
+        <DynamicEntry :entry="entry"
+          :schema="schema"></DynamicEntry>
+
+        <div class="mt-4 sm:mt-6 flex items-center space-x-4">
           <button type="submit"
             class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
             Update
@@ -65,7 +18,14 @@
             Delete
           </button>
         </div>
-      </form>
+      </FormRoot>
+
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import { entrySchema as schema, entryRules, entryInitial } from '~/appModules/EntSingle';
+
+const { data, entry } = useFormEntry(entryInitial, entryRules);
+</script>
