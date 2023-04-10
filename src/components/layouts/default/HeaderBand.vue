ff92a0ba85761b0c0817f626e89e898e7e27e2c4
Teerachai Laothong <tlaothong@gmail.com>
2023-04-10 23:48:30 +0700

    Replace images

--- src/components/layouts/default/HeaderBand.vue
+++ src/components/layouts/default/HeaderBand.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a href="https://github.com/tlaothong"
           class="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg"
+          <img src="/imgs/logobigdata.png"
             class="mr-3 h-6 sm:h-9"
             alt="Flowbite Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AppStarter</span>
