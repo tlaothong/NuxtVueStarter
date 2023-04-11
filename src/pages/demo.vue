59282bd9d10da56b27106aaf972440c370923825
Teerachai Laothong <tlaothong@gmail.com>
2023-04-11 11:33:45 +0700

    Improve demo

--- src/pages/demo.vue
+++ src/pages/demo.vue
@@ -2,8 +2,8 @@
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
       <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">จัดคนเข้าที่พัก</h2>
-        <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">เลือกคนสำหรับ พักห้อง {{ currentRoom }}</p>
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">จัดคนเข้าที่พัก (ห้องละ 2 ท่าน)</h2>
+        <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">เลือกคนสำหรับ บ้านพักหญิงหลังที่ 1 ชั้น 2 พักห้อง {{ currentRoom }}</p>
       </div>
       <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <div class="text-center text-gray-500 dark:text-gray-400"
